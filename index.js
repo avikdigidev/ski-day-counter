df4dd069efe92f00a2603b09693e9d10b6875f66
avikdigidev <avikdigidev@gmail.com>
2022-10-10 20:52:16 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const {ApolloServer, gql} = require("apollo-server");
+
+const typeDefs = gql`
+    type Query{
+    totalDays : Int!
+    }
+    `;
+
+/*const resolvers = {
+
+}*/
+
+const server = new ApolloServer({
+    typeDefs,
+    mocks: true
+})
+
+server.listen().then(({url}) => console.log(`Server running at ${url}`));
+
+
